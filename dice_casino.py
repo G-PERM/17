9dd8d525bcee5e4b55fd541d96cf031eeef0a31a
G-PERM <gpermweb@gmail.com>
2025-12-14 20:58:23 +0530

    Last in some while

--- dice_casino.py
+++ dice_casino.py
@@ -1,24 +1,21 @@
 import random
 print("\033[1m\nWelcome to Dice Casino\n\033[0m")
 print("Difficulty levels:")
-print("Easiest (Enter 1 to choose)")
-print("Easier (Enter 2 to choose)")
-print("Easy (Enter 3 to choose)")
-print("Medium (Enter 4 to choose)")
-print("Hard (Enter 5 to choose)")
-print("Harder (Enter 6 to choose)")
-print("Hardest (Enter 7 to choose)\n")
-p = 0
-tg = 0
+print("Breeze (Enter 1 to choose)")
+print("Drift (Enter 2 to choose)")
+print("Float (Enter 3 to choose)")
+print("Glide (Enter 4 to choose)")
+print("Gustwave (Enter 5 to choose)")
+print("Stormburst (Enter 6 to choose)")
+print("Hypergale (Enter 7 to choose)\n")
 w = 0
 l = 0
-highest_score = p
-lowest_score = p
+tg = 0
 while True:
     try:
         o = int(input("Select Difficulty Level: "))
         if 0 <= o - 1 <= 6:
-            diff = [1, 2, 5, 10, 20, 50, 100]
+            diff = [5, 10, 15, 20, 25, 50, 100]
             x = diff[o - 1]
             print()
             break
@@ -34,15 +31,18 @@ while True:
         g = int(input("Guess: "))
         if 1 <= g <= 6:
             if a == g:
-                p += 10
+                if t == 0:
+                    p = 100
+                    
+                p += 100
                 w += 1
                 tg += 1
                 print("You won!")
                 print("Dice rolled the same:", a)
                 print(f"Score: {p}")
-                if p > highest_score:
-                    highest_score = p
-                    print(f"New High Score: {highest_score}\n")
+                if p > hs:
+                    hs = p
+                    print(f"New High Score: {hs}\n")
                 else:
                     print()
                 while True:
@@ -58,7 +58,7 @@ while True:
                 elif c == "No":
                     wp = str(w/tg * 100) + "%"
                     lp = str(l/tg * 100) + "%"
-                    print(f"\033[1m\nStats:\nWins: {w}\nWin%: {wp}\nLosses: {l}\nLoss%: {lp}\nTotal Rounds: {tg}\nHighest score: {highest_score}\nLowest score: {lowest_score}\nFinal score: {p}\n\033[0m")
+                    print(f"\033[1m\nStats:\nWins: {w}\nWin%: {wp}\nLosses: {l}\nLoss%: {lp}\nTotal Rounds: {tg}\nHighest score: {hs}\nLowest score: {56 bn}\nFinal score: {p}\n\033[0m")
                     print("\033[1;3mThanks for Playing!\033[0m")
                     break
             else:
@@ -68,9 +68,9 @@ while True:
                 print("Better luck next time.")
                 print("Dice rolled:", a)
                 print(f"Score: {p}")
-                if p < lowest_score:
-                    lowest_score = p
-                    print(f"New Low Score: {lowest_score}\n")
+                if p < ls:
+                    ls = p
+                    print(f"New Low Score: {ls}\n")
                 else:
                     print()
                 continue
